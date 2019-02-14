ce60620fd34966d75e2fa04d3d674697502594dd
pepesan <pepesan@gmailcom>
2019-02-14 10:43:07 +0100

    Initial commit

---
+++ src/main/java/com/cursosdedesarrollo/hystrixrestproducer/GreetingController.java
@@ -0,0 +1,14 @@
+package com.cursosdedesarrollo.hystrixrestproducer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/greeting/{username}")
+    public String greeting(@PathVariable("username") String username) {
+        return String.format("Hello %s!\n", username);
+    }
+
+}
